91b5d1614f0b3cfcea1c0b6013e07ca7052f1305
CJ3016625 <CJ3016625@LAB01-PC04>
2023-09-20 12:51:52 -0300

    Adicionar arquivos de projeto.

---
+++ Loja_De_Roupas/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Loja_De_Roupas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.txbName = new System.Windows.Forms.TextBox();
+            this.txbPass = new System.Windows.Forms.TextBox();
+            this.txbPront = new System.Windows.Forms.TextBox();
+            this.mtxbPhone = new System.Windows.Forms.MaskedTextBox();
+            this.mtxbCPF = new System.Windows.Forms.MaskedTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(217, 98);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(49, 18);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Nome";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(201, 168);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(65, 18);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "Telefone";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(228, 204);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(38, 18);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "CPF";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.Location = new System.Drawing.Point(216, 237);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(50, 18);
+            this.lbl4.TabIndex = 3;
+            this.lbl4.Text = "Senha";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl5.Location = new System.Drawing.Point(189, 132);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(77, 18);
+            this.lbl5.TabIndex = 4;
+            this.lbl5.Text = "Prontuario";
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(652, 77);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 5;
+            this.btn2.Text = "INSERIR";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(667, 150);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 6;
+            this.btn3.Text = "INSERIR";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(349, 276);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(85, 30);
+            this.btn1.TabIndex = 7;
+            this.btn1.Text = "Salvar";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl6.Location = new System.Drawing.Point(317, 36);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(133, 33);
+            this.lbl6.TabIndex = 8;
+            this.lbl6.Text = "Cadastro";
+            this.lbl6.Click += new System.EventHandler(this.lbl6_Click);
+            // 
+            // txbName
+            // 
+            this.txbName.Location = new System.Drawing.Point(280, 96);
+            this.txbName.MaxLength = 100;
+            this.txbName.Name = "txbName";
+            this.txbName.Size = new System.Drawing.Size(241, 20);
+            this.txbName.TabIndex = 1;
+            // 
+            // txbPass
+            // 
+            this.txbPass.Location = new System.Drawing.Point(280, 235);
+            this.txbPass.MaxLength = 70;
+            this.txbPass.Name = "txbPass";
+            this.txbPass.PasswordChar = '*';
+            this.txbPass.Size = new System.Drawing.Size(241, 20);
+            this.txbPass.TabIndex = 5;
+            // 
+            // txbPront
+            // 
+            this.txbPront.Location = new System.Drawing.Point(280, 130);
+            this.txbPront.MaxLength = 10;
+            this.txbPront.Name = "txbPront";
+            this.txbPront.Size = new System.Drawing.Size(241, 20);
+            this.txbPront.TabIndex = 2;
+            // 
+            // mtxbPhone
+            // 
+            this.mtxbPhone.Location = new System.Drawing.Point(280, 166);
+            this.mtxbPhone.Mask = "(999) 00000-0000";
+            this.mtxbPhone.Name = "mtxbPhone";
+            this.mtxbPhone.Size = new System.Drawing.Size(241, 20);
+            this.mtxbPhone.TabIndex = 3;
+            this.mtxbPhone.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // mtxbCPF
+            // 
+            this.mtxbCPF.Location = new System.Drawing.Point(280, 202);
+            this.mtxbCPF.Mask = "000.000.000-00";
+            this.mtxbCPF.Name = "mtxbCPF";
+            this.mtxbCPF.Size = new System.Drawing.Size(241, 20);
+            this.mtxbCPF.TabIndex = 4;
+            this.mtxbCPF.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(794, 450);
+            this.Controls.Add(this.mtxbCPF);
+            this.Controls.Add(this.mtxbPhone);
+            this.Controls.Add(this.txbPront);
+            this.Controls.Add(this.txbPass);
+            this.Controls.Add(this.txbName);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.TextBox txbName;
+        private System.Windows.Forms.TextBox txbPass;
+        private System.Windows.Forms.TextBox txbPront;
+        private System.Windows.Forms.MaskedTextBox mtxbPhone;
+        private System.Windows.Forms.MaskedTextBox mtxbCPF;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Loja_De_Roupas/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Loja_De_Roupas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+
+            Connection connection = new Connection();
+            SqlCommand sqlCommand = new SqlCommand();
+
+            sqlCommand.Connection = connection.ReturnConnection();
+            sqlCommand.CommandText = @"INSERT INTO Usuario VALUES
+                (@ID, @Nome, @Prontuario, @Telefone, @CPF, @Senha)";
+
+            sqlCommand.Parameters.AddWithValue("@colunadatabela", txbName.Text);
+            sqlCommand.Parameters.AddWithValue("@colunadatabela", txbPront.Text);
+            sqlCommand.Parameters.AddWithValue("@colunadatabela", mtxbPhone.Text);
+            sqlCommand.Parameters.AddWithValue("@colunadatabela", mtxbCPF.Text);
+            sqlCommand.Parameters.AddWithValue("@colunadatabela", txbPass.Text);
+
+            sqlCommand.ExecuteNonQuery();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //{
+
+            //    if (validarForm())
+            //        Salvar();
+            //}
+
+            //    private void Salvar()
+            //    {
+            //        MessageBox.Show("Os dados foram salvos!");
+            //    }
+
+            //    private bool validarForm()
+            //    {
+            //        if (txbName.Text == "")
+            //        {
+            //            MessageBox.Show("Informe o Nome");
+            //            txbName.Focus();
+            //            return false;
+            //        }
+            //    else if (txbPront.Text == "")
+            //    {
+            //        MessageBox.Show("Informe o Prontuário");
+            //        txbPront.Focus();
+            //        return false;
+            //    }
+            //    else if (mtxbPhone.Text == "")
+            //    {
+            //        MessageBox.Show("Informe o Telefone");
+            //        mtxbPhone.Focus();
+            //        return false;
+            //    }
+            //    else if (mtxbCPF.Text == "")
+            //    {
+            //        MessageBox.Show("Informe o CPF");
+            //        mtxbCPF.Focus();
+            //        return false;
+            //    }
+            //    else if (txbPass.Text == "")
+            //    {
+            //        MessageBox.Show("Informe a Senha");
+            //        txbPass.Focus();
+            //        return false;
+            //    }
+            //    return true;
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //string name = txbName.Text;
+            //string enrollment = txbPront.Text;
+
+            //string message = "Nome: " + name +
+            //                 "\nMatrícula: " + enrollment;
+
+            //MessageBox.Show(
+            //    message,
+            //        "ATENÇÃO",
+            //        MessageBoxButtons.OK,
+            //        MessageBoxIcon.Information
+            //       );
+            //}
+        }
+}
